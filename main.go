9dd3f1b64072e3738b2f3b1cf7daf4f3cd829192
senabi <sensenabi@gmail.com>
2022-04-29 00:16:53 -0500

    feat: wc clone

--- main.go
+++ main.go
@@ -9,37 +9,29 @@ import (
 )
 
 func main() {
+	// get args
+	args := os.Args[1:]
 	file, err := os.Open("./generator/lorem.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	wordsMap := make(map[string]int)
 
-	scanner := bufio.NewScanner(file)
+	target := args[0]
 	ix := int64(1)
+	occurencies := int64(0)
+	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		linestring := scanner.Text()
 		for _, word := range strings.Split(linestring, " ") {
 			for _, w := range strings.Split(word, ".") {
-				wordsMap[w]++
+				if w == target {
+					occurencies++
+				}
 			}
 		}
 		ix++
 	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("lines:", ix)
-	fmt.Println("unique words:", len(wordsMap))
-
-	file, err = os.Create("./words.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	for k, v := range wordsMap {
-		fmt.Fprintln(file, k, v)
-	}
+	fmt.Println(target, ":", occurencies)
 }

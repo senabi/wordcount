c67b0397c14b92f8810e37717618649a708e7000
senabi <sensenabi@gmail.com>
2022-04-28 23:04:04 -0500

    feat: wordcount

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.Open("./generator/lorem.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	wordsMap := make(map[string]int)
+
+	scanner := bufio.NewScanner(file)
+	ix := int64(1)
+	for scanner.Scan() {
+		linestring := scanner.Text()
+		for _, word := range strings.Split(linestring, " ") {
+			for _, w := range strings.Split(word, ".") {
+				wordsMap[w]++
+			}
+		}
+		ix++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("lines:", ix)
+	fmt.Println("unique words:", len(wordsMap))
+
+	file, err = os.Create("./words.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	for k, v := range wordsMap {
+		fmt.Fprintln(file, k, v)
+	}
 }
